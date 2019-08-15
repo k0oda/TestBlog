733874917fa3a80292334a7b779ae3996b231c54
kukree <kulevgen32@gmail.com>
2019-08-16 03:46:00 +0600

    styling: update new_post template styling

--- TestBlog/blog/forms.py
+++ TestBlog/blog/forms.py
@@ -3,6 +3,12 @@ from .models import Post
 
 
 class PostForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['author'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Автор'})
+        self.fields['title'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Название поста'})
+        self.fields['text'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Текст'})
+
     class Meta:
         model = Post
         exclude = ('publication_date',)
