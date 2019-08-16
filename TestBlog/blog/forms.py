73fc4889a543324facb051ab7d5abac2731c0af8
kukree <kulevgen32@gmail.com>
2019-08-16 21:01:16 +0600

    add: read-only marker for author in editing post

--- TestBlog/blog/forms.py
+++ TestBlog/blog/forms.py
@@ -9,6 +9,12 @@ class PostForm(ModelForm):
         self.fields['title'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Название поста'})
         self.fields['text'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Текст'})
 
+        try:
+            if kwargs['instance']:
+                self.fields['author'].widget.attrs.update({'readonly': ''})
+        except KeyError:
+            pass
+
     class Meta:
         model = Post
         exclude = ('publication_date', 'publication_time')
